a147115b772ffedabdc85f1d0da1cde95faeb661
Peter Milne <peter.milne@d-tacq.com>
2025-10-16 19:11:36 +0100

    pdmean works

--- pdmean.cpp
+++ pdmean.cpp
@@ -8,17 +8,20 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define NCHAN 8
-#define DECIM 1024         // must be power of two
+#define DEF_NCHAN 8
+#define DEF_DECIM 1024         // must be power of two
 #define SHR   10           // divide by 1024
 #define ADC   short
 
 namespace G {
-	int decim = DECIM;
-	int nchan = NCHAN;    // channels are int16
+	int decim = DEF_DECIM;
+	int nchan = DEF_NCHAN;    // channels are int16
 	int nspad = 0;        // spads are int32
 }
 
+#define NCHAN	G::nchan
+#define DECIM	G::decim
+#define NSPAD	G::nspad
 #define BUFFER_WIDTH   (G::nchan+G::nspad*2)
 #define BUFFER_SAMPLES (G::decim * BUFFER_WIDTH)
 #define BUFFER_SZ      (BUFFER_SAMPLES*sizeof(ADC))
@@ -39,8 +42,12 @@ void process(ADC* buffer, long* sum, ADC* decim)
 		decim[ic] = sum[ic] >> SHR;
 	}
 	// copy the spads across
-	for (; ic < BUFFER_WIDTH; ++ic ){
-		decim[ic] = buffer[DECIM/2*BUFFER_WIDTH + ic];
+	bp -= BUFFER_WIDTH;
+	long* lbp = (long*)(bp+NCHAN);
+	long* ldecim = (long*)(decim+NCHAN);
+
+	for (int ispad = 0; ispad < NSPAD; ++ispad){
+		ldecim[ispad] = lbp[ispad];
 	}
 }
 
@@ -52,6 +59,10 @@ void ui(int argc, char* argv[])
 			exit(1);
 		}
 		G::decim = atoi(argv[1]);
+		if (G::decim == 0){
+			fprintf(stderr, "pdmean [decim [nchan [nspad]]]\n");
+			exit(1);
+		}
 	if (argc > 2)
 		G::nchan = atoi(argv[2]);
 	if (argc > 3)
@@ -63,7 +74,7 @@ int main(int argc, char* argv[])
 	ui(argc, argv);
 	ADC * buffer = new ADC[BUFFER_SAMPLES];
 	long* sum = new long[NCHAN];
-	ADC * decim = new ADC[NCHAN];
+	ADC * decim = new ADC[BUFFER_WIDTH];
 
 	int rc = setvbuf(stdin, (char*)buffer, _IOFBF, BUFFER_SZ);
 	assert(rc == 0);
@@ -74,7 +85,7 @@ int main(int argc, char* argv[])
 		int nwords = fread(buffer, sizeof(ADC), BUFFER_SAMPLES, stdin);
 		if (nwords == BUFFER_SAMPLES){
 			process(buffer, sum, decim);
-			fwrite(decim, sizeof(ADC), NCHAN, stdout);
+			fwrite(decim, sizeof(ADC), BUFFER_WIDTH, stdout);
 			fflush(stdout);
 		}else{
 			fprintf(stderr, "ERROR fread returns %ld\n", nwords);
